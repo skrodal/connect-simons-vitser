6ec0c3a01536cb19886bbb6ede810bafd52c9d1e
Simon Skrødal <simon.skrodal@gmail.com>
2015-08-20 11:56:50 +0930

    1st Commit

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * Simons Vitser
+ *
+ * Et enkelt demo-API for å demonstrere UNINETT Connect Tjenesteplattform.
+ *
+ * @author Simon Skrødal
+ * @since August 2015
+ */ 
+
+
+
+/* 
+ * $_SERVER er en array som bl.a. inneholder headere og autentiserings-info 
+ * som er svært nyttig å benytte seg av.
+ *
+ * Dersom forespørsel kommer via Connect API Gatekeeper kan vi bl.a. se disse 
+ * headerene om klient og bruker:
+ *
+ *       "HTTP_X_FEIDECONNECT_CLIENTID": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+ *       "HTTP_X_FEIDECONNECT_SCOPES": "api_scope_1,api_scope_2,api_scope_3,...,...",
+ *       "HTTP_X_FEIDECONNECT_USERID": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+ *       "HTTP_X_FEIDECONNECT_USERID_SEC": "feide:simon@uninett.no",
+ *
+ * Spesielt scopes og brukerinfo over er byttig for å styre tilgang. 
+ * En klient kan ha tilgang til 1-mange scopes. Klienten kan også bestemme seg for redusere/øke scopes i 
+ * sin request basert på hvilken bruker som er logget på. 
+ * 
+ * I tillegg følger autentiseringsinfo fra Connect som vi kan bruke for å i det hele tatt tillate tilgang:
+ *
+ *       "PHP_AUTH_USER": "feideconnect",
+ *       "PHP_AUTH_PW": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+ *
+ * USER/PW for ditt API finner du i Connect Dashboard under meny "trust"
+ */
+
+// Vi snakker JSON
+header('Content-Type: application/json');
+// Forventet bruker/passord - lagre i en config på et trygt sted:
+$apiUser = 'feideconnect';
+$spiPass = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+
+
+
+// 1. 	La oss først sjekke om forespørsel kommer fra Connect GK med riktig bruker/pass
+// 		Dersom PHP_AUTH_USER ikke er satt i det hele tatt kan vi like gjerne stoppe her
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="Simons Vitsekolleksjon"');
+    header('HTTP/1.0 401 Unauthorized');
+    // Gi en feilmelding
+    exit(json_encode(array('status' => false, 'message' => 'HTTP/1.0 401 Unauthorized: Access requires Connect API GK Credentials')));
+}
+
+
+
+
+// 2.	Sjekk at bruker/passord stemmer overens
+if(strcmp($_SERVER['PHP_AUTH_USER'], $apiUser) !== 0 || strcmp($_SERVER['PHP_AUTH_PW'], $apiPass) !== 0) {
+	// Feilmelding hvis ikke
+	exit(json_encode(array('status' => false, 'message' => 'HTTP/1.0 401 Unauthorized: Invalid Credentials')));
+}
+
+
+// 3. 	Vi er inne! 
+//		Vitse-APIet er definert i Dashboard med følgende scopes:
+//			- A (alle har tilgang)	: 	gk_simons-vitser
+//			- 9-årsgrense ('9')		:	gk_simons-vitser_9
+//			- 15-årsgrense ('15')	:	gk_simons-vitser_15
+//			- 18-årsgrense ('18')	:	gk_simons-vitser_18
+//
+//		Scopes som kommer inn i header dropper prefiks 'gk_simons-vitser' og vi ender opp med '9', '15', '18'
+
+// La meg dra ut mine beste vitser:
+$vitser = array(
+	// Familievennlig
+	array(
+			'[A]: Hørt om laksen som mista strømmen?',
+			'[A]: Har dere hørt om torsken som var lei sei?',
+			'[A]: Har du hørt om han som hadde bakteriefobi? Han kokte isbitene før han puttet dem i drikken sin :D',
+		),
+	// Aldersgrense 9 år
+	array(
+			'[9]: Det er bedre med 10 fulger på taket enn ei ugle i potetmosen.',
+			'[9]: Hørt om kokken som var så dårlig i fotball? Sleivspark hver eneste gang.',
+			'[9]: Hørt om elektrikeren som reiste til syden for å koble av...?',
+			'[9]: Har du hørt om kona som vekket mannen midt på natten? Han hadde jo glemt å ta sovemedisinen sin...',
+		),
+	// Aldersgrense 15 år
+	array(
+			'[15]: Har du hørt om grisen som hadde svin på skogen?',
+			'[15]: Visste du at Trine Hattestad er veldig spydig?',
+			'[15]: Har du hørt om mannen som var så fornøyd med at svigermor bodde kun et steinkast unna? - Før eller siden må han jo treffe...',
+			'[15]: Har du hørt om når Jesus var med i svømmekonkurranse? - Han vant på "walk-over".',
+		),
+	// Aldersgrense 18 år
+	array(
+			'[18]: Har du hørt om kannibalen som dreit ut broren sin?',
+			'[18]: Pappa, hva er en transvestitt? - Spør tante Erik...',
+		),
+	);
+
+
+// 4. 	Sjekk hvilke scopes (aldersgrense) klienten har tilgang til (ingen eller flere av 9, 15, 18). 
+//		Dersom ett eller flere scopes, konverter den komma-separerte String'en til en array:
+$aldersgrenser = empty($_SERVER['HTTP_X_FEIDECONNECT_SCOPES']) ? array() : explode(',', $_SERVER['HTTP_X_FEIDECONNECT_SCOPES']);
+
+
+// 5. 	Start jobben med å finne en passende vits iht. godkjente aldersgrenser
+//		Først, tilfeldig aldersnivå (nivå 1 i $vitser-array): 
+$vitsescope = rand(0, sizeof($aldersgrenser));
+//		Så, velg en konkret vits innenfor denne aldersgrensen $vitser[x][rand]:
+$vits = $vitser[ $vitsescope ][ rand(0, sizeof($vitser[$vitsescope])-1 )];
+
+// 6. Returner vår tilfeldig valgte vits og ferdig.
+exit( 	json_encode( 
+			array(
+			  'status' => true, 
+			  'vits' => $vits
+			)
+		)
+	);
+
+
